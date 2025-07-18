1f1a540a27bf00c23626ec3cd99f4c2e89d8b1f2
aima-00 <zhuquanshuai2022@163.com>
2025-07-18 14:24:28 +0800

    可以显示消息发送者IP地址和端口号

--- chat_server.c
+++ chat_server.c
@@ -13,7 +13,7 @@
 int client_sockets[MAX_CLIENTS] = {0};
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *handle_client(void *arg) {
+void *handle_client(void *arg) {            // 创建一个线程处理客户端。后面可以增加参数用户名
     int client_socket = *(int *)arg;        // 客户端套接字 数组形式的
     char buffer[BUFFER_SIZE];               //客户端接收缓冲区
     int read_size;
@@ -27,13 +27,26 @@ void *handle_client(void *arg) {
         }
 
         buffer[read_size] = '\0';
-        printf("Received %d: %s\n",client_socket, buffer);
+       // 获取客户端地址信息
+        struct sockaddr_in client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+    char client_ip[INET_ADDRSTRLEN];        // 用于存储客户端IP地址字符串
+    int client_port ;
+    //打印接收到的消息,并显示套接字
+    if (getpeername(client_socket, (struct sockaddr*)&client_addr, &addr_len) == 0) {
+        inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN);
+        client_port = ntohs(client_addr.sin_port);      // 获取客户端端口号
+        // 打印带端口的消息
+        printf("Received from %s:%d: %s\n", client_ip, client_port, buffer);
+    }
 
         // 广播消息给所有客户端
         pthread_mutex_lock(&mutex);//锁定互斥锁，确保线程安全操作客户端数组。
         for (int i = 0; i < MAX_CLIENTS; i++) {
             if (client_sockets[i] != 0 && client_sockets[i] != client_socket) {
-                send(client_sockets[i], buffer, strlen(buffer), 0);
+                char broadcast_msg[BUFFER_SIZE + 50];
+                sprintf(broadcast_msg, "[%s:%d] %s", client_ip, client_port, buffer);
+                send(client_sockets[i], broadcast_msg, strlen(broadcast_msg), 0);
             }
         }
         pthread_mutex_unlock(&mutex);
@@ -59,13 +72,21 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int port = atoi(argv[1]);
+    int port = atoi(argv[1]);       //程序会将 port 设置为 8080，然后服务器会在该端口监听客户端连接请求。
+    //server_socket：服务器监听的主套接字。
+    // client_socket：每次接受新客户端连接时返回的客户端套接字。
+    // 用途：通过这些描述符可以对网络连接进行操作，如发送、接收、关闭等。
     int server_socket, client_socket;
+    // server_addr：保存服务器自身的地址信息（IP + 端口）。
+    // client_addr：保存连接到的客户端地址信息。
     struct sockaddr_in server_addr, client_addr;
+    // socklen_t：用于存储地址结构的长度。
     socklen_t client_len = sizeof(client_addr);
+    // 线程ID
     pthread_t thread_id;
 
-    // 创建服务器套接字
+    // 创建服务器套接字 程序通过系统调用请求操作系统创建的
+    // 相当于文件描述符，通过它可以进一步对网络连接进行操作。
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
         perror("Socket creation failed");
@@ -73,17 +94,23 @@ int main(int argc, char *argv[]) {
     }
 
     // 配置服务器地址
+    //一台机器可能有多个IP地址，127.0.0.1 本机回环地址。192.168.1.1 局域网IP
+    //配置服务器地址是为了告诉操作系统服务器要监听本机哪个 IP 和端口上的连接请求，是服务器网络通信的必要准备步骤。
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(port);
 
     // 绑定套接字
+    //将服务器套接字与指定的地址和端口绑定。这样，通过套接字，操作系统就可以接收来自指定地址和端口的连接请求。
     if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
         perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
-    // 监听连接
+    // 让服务器处于监听状态，先listen，然后accept
+    //listen() 函数使服务器套接字进入监听状态(无法主动发起连接，只能等待连接)，等待客户端连接请求。
+    //第二个参数指定了服务器可以排队等待的最大连接数。
+    //如果有多个客户端同时连接，超过这个数量的连接请求将被拒绝。
     if (listen(server_socket, 5) < 0) {
         perror("Listen failed");
         exit(EXIT_FAILURE);
@@ -93,6 +120,9 @@ int main(int argc, char *argv[]) {
 
     while (1) {
         // 接受新连接
+        //accept() 函数会阻塞，直到有客户端连接到服务器。
+        //一旦有客户端连接，accept() 会返回一个新的套接字描述符 client_socket，用于与该客户端进行通信。
+        //同时，client_addr 会被填充为连接的客户端的地址信息。
         client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
         if (client_socket < 0) {
             perror("Accept failed");
